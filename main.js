db2aa16ac7b99db78a99589b0b6c3f52dd1fe425
holmanhub <holman.eric85@gmail.com>
2017-08-28 18:48:37 -0500

    add json files and linked to js

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("sup")
+console.log("linked to main.js");
+var dogs = [];
+var breeds = [];
+
+function runDogs(){	
+	var data = JSON.parse(this.responseText);
+	console.log("dogs", data);
+}
+
+function runBreeds(){
+	var data = JSON.parse(this.responseText);
+	console.log("breeds", data);
+}
+
+function itBroke(){
+	console.log("It Broke");
+}
+
+var myDogs = new XMLHttpRequest();
+myDogs.addEventListener('load', runDogs);
+myDogs.addEventListener('error', itBroke);
+myDogs.open('GET', 'dogs.json');
+myDogs.send();
+
+var myBreeds = new XMLHttpRequest();
+myBreeds.addEventListener('load', runBreeds);
+myBreeds.addEventListener('error', itBroke);
+myBreeds.open('GET', 'breeds.json');
+myBreeds.send();
